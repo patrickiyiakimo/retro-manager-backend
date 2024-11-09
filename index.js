a1d09a34b87576d1059fdc765c4585b840ee3892
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-11-09 21:43:01 +0100

    added a path resolve

--- index.js
+++ index.js
@@ -57,38 +57,26 @@
 
 
 
-
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const { v4: uuidv4 } = require("uuid");
 require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 2500;
 
-// const corsOptions = {
-//   origin: "http://localhost:3000",
-//   methods: ["GET", "POST", "PUT", "DELETE"],
-//   allowedHeaders: ["Content-Type", "Authorization"],
-//   credentials: true,
-// };
-
-// app.use(cors(corsOptions));
-
-// app.options("*", cors(corsOptions));
-
 app.use(cors());
 app.options("*", cors());
-
 app.use(express.json());
 
 // Routes
-app.use("/register", require("./routes/register"));
-app.use("/login", require("./routes/login"));
-app.use("/standups", require("./routes/standups"));
-app.use("/invites", require("./routes/invites"));
-app.use("/dashboard", require("./routes/dashboard"));
-app.use("/generateId", require("./routes/generateId"));
+app.use("/register", require(path.resolve(__dirname, "./routes/register")));
+app.use("/login", require(path.resolve(__dirname, "./routes/login")));
+app.use("/standups", require(path.resolve(__dirname, "./routes/standups")));
+app.use("/invites", require(path.resolve(__dirname, "./routes/invites")));
+app.use("/dashboard", require(path.resolve(__dirname, "./routes/dashboard")));
+app.use("/generateId", require(path.resolve(__dirname, "./routes/generateId")));
 
 app.get("/", (req, res) => {
   res.send("Hello from Retro Manager!");
@@ -97,7 +85,7 @@ app.get("/", (req, res) => {
 // Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send("Something broke!");
+  res.status(500).json({ error: "Internal Server Error", message: err.message });
 });
 
 // Export the app for Vercel
