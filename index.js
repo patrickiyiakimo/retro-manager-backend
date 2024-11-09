2b99fa55de7faf0fcee5c119cc7aa7c9209f1b1b
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-11-09 21:38:42 +0100

    added vercel file and made it serverless

--- index.js
+++ index.js
@@ -1,3 +1,63 @@
+// const express = require("express");
+// const cors = require("cors");
+// const { v4: uuidv4 } = require("uuid");
+// require("dotenv").config();
+
+// const app = express();
+// const PORT = process.env.PORT || 2500;
+
+
+// // const corsOptions = {
+// //   origin: "http://localhost:3000",
+// //   methods: ["GET", "POST", "PUT", "DELETE"],
+// //   allowedHeaders: ["Content-Type", "Authorization"],
+// //   credentials: true,
+// // };
+
+
+// // app.use(cors(corsOptions));
+
+
+// // app.options("*", cors(corsOptions));
+
+// app.use(cors());
+// app.options("*", cors());
+
+// app.use(express.json());
+
+// // Routes
+// app.use("/register", require("./routes/register"));
+// app.use("/login", require("./routes/login"));
+// app.use("/standups", require("./routes/standups"));
+// app.use("/invites", require("./routes/invites"));
+// app.use("/dashboard", require("./routes/dashboard"));
+// app.use("/generateId", require("./routes/generateId"));
+
+// app.get("/", (req, res) => {
+//   res.send("Hello from Retro Manager!");
+// });
+
+// // Error handling middleware
+// app.use((err, req, res, next) => {
+//   console.error(err.stack);
+//   res.status(500).send("Something broke!");
+// });
+
+
+// app.listen(PORT, () => {
+//   console.log(`Express server running at http://localhost:${PORT}/`);
+// });
+
+
+
+
+
+
+
+
+
+
+
 const express = require("express");
 const cors = require("cors");
 const { v4: uuidv4 } = require("uuid");
@@ -6,7 +66,6 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 2500;
 
-
 // const corsOptions = {
 //   origin: "http://localhost:3000",
 //   methods: ["GET", "POST", "PUT", "DELETE"],
@@ -14,9 +73,7 @@ const PORT = process.env.PORT || 2500;
 //   credentials: true,
 // };
 
-
 // app.use(cors(corsOptions));
-
 
 // app.options("*", cors(corsOptions));
 
@@ -43,7 +100,5 @@ app.use((err, req, res, next) => {
   res.status(500).send("Something broke!");
 });
 
-
-app.listen(PORT, () => {
-  console.log(`Express server running at http://localhost:${PORT}/`);
-});
+// Export the app for Vercel
+module.exports = app;
