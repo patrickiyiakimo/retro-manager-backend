98e1af79993901ee3ca433da95fdb45b678f9a91
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-11-20 12:22:45 +0100

    made app.js the sub-file

--- config/db.js
+++ config/db.js
@@ -10,32 +10,3 @@ const pool = new Pool({
 });
 
 module.exports = pool;
-
-
-
-
-
-
-
-
-// require("dotenv").config();
-// const { Pool } = require("pg");
-
-// // Configure the database connection
-// const pool = new Pool({
-//   connectionString: process.env.POSTGRES_URL, // Use the connection string from the .env file
-//   ssl: {
-//     rejectUnauthorized: false, // Required for Neon and most hosted PostgreSQL databases
-//   },
-// });
-
-// // Test the database connection
-// pool
-//   .connect()
-//   .then(() => console.log("Connected to the database successfully"))
-//   .catch((err) => {
-//     console.error("Database connection error:", err);
-//     process.exit(1); // Exit the application if the database connection fails
-//   });
-
-// module.exports = pool;
