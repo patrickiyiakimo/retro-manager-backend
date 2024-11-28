69c1f87dbd183a341dee9931aa2faa9d64f6692a
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-11-28 12:04:39 +0100

    cleared some dirty comments

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -5,22 +5,17 @@ require("dotenv").config();
 
 const handleNewUser = async (req, res) => {
   const { username, password, email } = req.body;
-  //validate user
   if (!username || !email || !password) {
     return res.status(400).json({ message: "Username and email and password are required" });
   }
-
-  //check if email already exist
   const user = await pool.query("SELECT * FROM users WHERE email = $1", [email]);
   if (user.rows.length > 0) {
     return res.sendStatus(409); //conflict.
   }
 
   try {
-    //hash password
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    //insert the new user into the database
     const newUser = await pool.query(
       "INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING *",
       [username, email, hashedPassword]
