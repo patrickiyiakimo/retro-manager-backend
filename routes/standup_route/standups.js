20704b573597f61c2f214df3c5c5f64070fd816a
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2025-02-27 13:44:16 +0100

    Feat: Created new folders for the files

--- routes/standup_route/standups.js
+++ routes/standup_route/standups.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const pool = require("../config/db");
+const pool = require("../../config/db");
 const standupsControllers = require("../controllers/standupsControllers");
 
 router.post("/", standupsControllers.newStandups);
